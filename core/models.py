40ec9a47e3832ecad4ca69babcfe18080c75a9f2
Harsh Kumar Singh <66480262+Harsh-the-Dev@users.noreply.github.com>
2026-01-17 17:11:09 +0530

    updated model

--- core/models.py
+++ core/models.py
@@ -25,8 +25,7 @@ class Attendance(models.Model):
     date = models.DateField()
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
 
-    class Meta:
-        unique_together = ('employee', 'date')  
+     
 
     def __str__(self):
         return f"{self.employee.full_name} - {self.date} - {self.status}"
